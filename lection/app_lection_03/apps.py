9c3336a0fee34728bbfa6913441b4b8a2f6ae58a
FlagmanMax <borodov.m@gmail.com>
2024-04-23 22:18:18 +0300

    Django_lections

---
+++ lection/app_lection_03/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppLection03Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_lection_03'
---
+++ lection/app_seminar_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppSeminar1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_seminar_1'
